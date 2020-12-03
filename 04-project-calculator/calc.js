e8c33dc2e0af63686e5bd1268ce400b9267c3fff
sanghaarsha <sanghaarsha@gmail.com>
2020-12-03 15:56:01 +0545

    calculator complete

--- 04-project-calculator/calc.js
+++ 04-project-calculator/calc.js
@@ -1,11 +1,30 @@
 const path = require("path");
+const bodyParser = require("body-parser");
+
 const express = require("express");
-const app = express();
 const port = 3030;
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-    res.send("Hello");
+    res.sendFile(path.join(__dirname, "index.html"));
 });
+
+app.post("/", (req, res) => {
+    jsonFormData = req.body;
+    num1 = parseInt(jsonFormData.num1);
+    num2 = parseInt(jsonFormData.num2);
+    if (isNaN(num1) || isNaN(num2)) {
+        res.send("Invalid Input!!");
+    } else {
+        res.send(
+            `Num1 = ${num1}\nNum2 = ${num2}\n\n ${num1}\n+${num2}\n ---\n ${
+                num1 + num2
+            }`
+        );
+    }
+});
+
 app.listen(port, () => {
-    console.log(`App Open in http://localhost:${port}`);
+    console.log(`App open in http://localhost/${port}`);
 });
